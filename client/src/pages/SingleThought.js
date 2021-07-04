037c7d797b2009026bfca241fd5846d9503f0033
Celina Louissaint <celinalouissaint@gmail.com>
2021-07-03 18:47:19 -0700

    error with useParams hook

---
+++ client/src/pages/SingleThought.js
@@ -0,0 +1,26 @@
+import { useParams } from 'react-router-dom';
+import React from 'react';
+
+
+const SingleThought = (props) => {
+  const { id: thoughtId } = useParams();
+  console.log(thoughtId);
+
+  return (
+    <div>
+      <div className="card mb-3">
+        <p className="card-header">
+          <span style={{ fontWeight: 700 }} className="text-light">
+            Username
+          </span>{' '}
+          thought on createdAt
+        </p>
+        <div className="card-body">
+          <p>Thought Text</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleThought;
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
@@ -23,6 +24,19 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+ 
+// ----- need this below to deploy to heroku ----- //
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+
+// ----- need this above to deploy to heroku ----- //
 
 db.once('open', () => {
   app.listen(PORT, () => {
